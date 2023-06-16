af16f575ce9db670a976ae54e5769df8c8b783f0
Arthur Albuquerque <85767234+Arthurosu@users.noreply.github.com>
2023-06-16 13:41:45 -0300

    Add files via upload

---
+++ Python_art/input_output.py
@@ -0,0 +1,8 @@
+# Output é aquilo que vem de dentro do código para que o usuário possa ver.
+print("Hello World")
+
+# Input é aquilo que vem de fora do código para dentro dele.
+nome = input("Insira seu nome ")
+
+# Você pode trazer algo que você colocou dentro do Código(input) para fora novamente(output)
+print(nome)
---
+++ Python_art/variaveis.py
@@ -0,0 +1,24 @@
+# Variáveis são como caixinhas que guardam informação.
+
+# Existem vários tipos de variáveis, cada uma preparada para guardar um tipo diferente de informação
+
+# variável de texto -> String:
+# Strings são variáveis que guardam letras, números e caracteres e
+# são usadas para gaurdar textos.
+texto = "Oi, eu sou uma string"
+
+# variável de número inteiro -> int:
+# Integers são variáveis que guardam números inteiros.
+# podendo ser negativos ou positivos.
+numero_inteiro = 5
+
+# variável de número quebrado -> float:
+# Floats são variáveis que guardam números "quebrados"(Reais).
+# podendo ser negativos ou positivos.
+numero_real = 5.5
+
+# variável de verdadeiro ou falso -> Boolean:
+# Variáveis booleanas guardam valores que podem ser verdadeiro ou falso.
+verdadeiro_falso = True
+
+
